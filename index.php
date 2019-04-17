9eec2fcf20da537b82dcc9159249cb51eddb5051
张景敏 <1036853357@qq.com>
2019-04-17 17:45:39 +0800

    swoole入门代码

--- index.php
+++ index.php
@@ -6,4 +6,12 @@
  * Date:  2019/4/15
  * Time:  15:42
  */
-echo "Yeah, This Is Supper Man";
+$http = new swoole_http_server("0.0.0.0", 9501);
+
+$http->on('request', function ($request, $response) {
+    var_dump($request->get, $request->post);
+    $response->header("Content-Type", "text/html; charset=utf-8");
+    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+});
+
+$http->start();
