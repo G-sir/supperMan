d9f31cb5734177018ee16b6a678a6c428fabeee8
张景敏 <1036853357@qq.com>
2019-04-19 14:24:08 +0800

    学习 swoole

--- index.php
+++ index.php
@@ -6,12 +6,23 @@
  * Date:  2019/4/15
  * Time:  15:42
  */
-$http = new swoole_http_server("0.0.0.0", 9501);
-
-$http->on('request', function ($request, $response) {
-    var_dump($request->get, $request->post);
-    $response->header("Content-Type", "text/html; charset=utf-8");
-    $response->end("<h1>Hello Swoole. #".rand(1000, 9999)."</h1>");
+echo "author:changwei [www.changwei.me]\nEmail:867597730@qq.com\nstart at ".time()."\n";
+//官网demo
+$server = new swoole_websocket_server("0.0.0.0", 9501);
+$server->on('open', function (swoole_websocket_server $server, $request) {
+    echo "server: handshake success with fd{$request->fd}\n";//$request->fd 是客户端id
 });
-
-$http->start();
+$server->on('message', function (swoole_websocket_server $server, $frame) {
+    echo "receive from {$frame->fd}:{$frame->data},opcode:{$frame->opcode},fin:{$frame->finish}\n";
+    //$frame->fd 是客户端id，$frame->data是客户端发送的数据
+    //服务端向客户端发送数据是用 $server->push( '客户端id' ,  '内容')
+    $data = $frame->data;
+    foreach($server->connections as $fd){
+        $server->push($fd , $data);//循环广播
+    }
+});
+$server->on('close', function ($ser, $fd) {
+    echo "client {$fd} closed\n";
+});
+$server->start();
+?>
